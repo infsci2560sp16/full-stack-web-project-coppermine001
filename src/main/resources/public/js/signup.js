1210841269797cc64a9538bfb0e4a0f5c7b134e9
coppermine001 <jit20@pitt.edu>
2016-02-17 23:37:02 -0500

    1.3
    1.3

--- src/main/resources/public/js/signup.js
+++ src/main/resources/public/js/signup.js
@@ -5,7 +5,7 @@ return (('0'<=cCheck) && (cCheck<='9'));
 
 function IsAlpha(cCheck)
 {
-return ((('a'<=cCheck) && (cCheck<='z')) || (('A'<=cCheck) && (cCheck<='Z')))
+return ((('a'<=cCheck) && (cCheck<='z')) || (('A'<=cCheck) && (cCheck<='Z')));
 }
 
 function IsValid()
@@ -23,12 +23,12 @@ return true;
 }
 function chkEmail(str)
 {
-return str.search(/[\w\-]{1,}@[\w\-]{1,}\.[\w\-]{1,}/)==0?true:false
+return str.search(/[\w\-]{1,}@[\w\-]{1,}\.[\w\-]{1,}/)===0?true:false;
 }
 
 function docheck()
 {
-if(reg.UserName.value=="")
+if(reg.UserName.value==="")
 {
 alert("Please enter valid username");
 return false;
@@ -48,12 +48,12 @@ else if(reg.UserPassword.value != reg.CUserPassword.value)
 alert("the reentered password is not correct");
 return false;
 }
-else if(reg.NickName.value =="")
+else if(reg.NickName.value ==="")
 {
 alert("Please enter nickname");
 return false;
 }
-else if(reg.Email.value =="")
+else if(reg.Email.value ==="")
 {
 alert("Please enter email address");
 return false;
